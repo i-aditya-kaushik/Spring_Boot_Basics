8026003adfbdc29f4d652e64f58668e5ad005805
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-06-27 23:05:15 +0530

    Web Application 4 [ModelAndView]

--- Web_Application/src/main/java/com/example/demo/HomeController.java
+++ Web_Application/src/main/java/com/example/demo/HomeController.java
@@ -6,16 +6,18 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
-	public String home(HttpServletRequest req,HttpServletResponse res) {
+	public ModelAndView home(@RequestParam("name")String myName) {
 //		System.out.println("Request is accepted successfully");
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println("Hi "+ name);
-		session.setAttribute("name", name);
-		return "home";
+		ModelAndView mv = new ModelAndView();
+//		System.out.println("Hi "+ myName);
+		mv.addObject("name",myName);
+		mv.setViewName("home");
+		return mv;
 	}
 }
