dca2e711f9aefdd22c1c6968e6285e150ec53bfa
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2021-06-29 08:33:24 +0530

    Spring Data REST

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'Adi','Python');
+insert into alien values(102,'Sid','Cpp');
+insert into alien values(103,'Yuvi','Python');
+insert into alien values(104,'Isha','Java');
+insert into alien values(105,'Altair','Python');
